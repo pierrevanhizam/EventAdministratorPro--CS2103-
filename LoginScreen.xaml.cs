de83b442c3ed629db159115bed25712c03c1f3c5
Melvin Lee <zy@zy.sg>
2012-03-04 23:42:56 +0800

    Moved main screen logic from MainWindow.xaml to LoginScreen.xaml. Implemented XAML layout for LoginScreen.xaml.

---
+++ LoginScreen.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+using EventClass;
+
+namespace EMP
+{
+	public partial class LoginScreen
+	{
+		private List<Event> _upcomingEventsList;
+		
+		public LoginScreen()
+		{
+			this.InitializeComponent();
+			
+			// Generate 'Upcoming Events' from flat file for now.
+			this._upcomingEventsList = new List<Event>();
+			this.ImportEventsFromFile();
+			this.upcomingEventsListBox.ItemsSource = this._upcomingEventsList;
+		}
+		
+		// Method for getting events from DB.
+		private void GetUpcomingEvents(string startDate, int startIndex, int number)
+		{
+			// TODO: Get events based on startDate and return x number of events starting from startIndex.
+		}
+		
+		// Method to validate user login information.
+		private void ValidateUser(string username, string password)
+		{
+			// TODO: Search for existing user information from database.
+		}
+		
+		// Import events from flat-file (events.txt).
+		private void ImportEventsFromFile()
+		{
+			try
+			{
+				StreamReader sr = new StreamReader("events.txt");
+				while (!sr.EndOfStream)
+				{
+					int id = Convert.ToInt16(sr.ReadLine());
+                    string name = sr.ReadLine();
+                    string description = sr.ReadLine(); 
+                    string startDate = sr.ReadLine();
+                    string organizerName = sr.ReadLine();
+					
+					Event i = new Event(id, name, description, startDate, organizerName);
+                    this._upcomingEventsList.Add(i);
+				}
+			}
+			catch (IOException e)
+            {
+                // Display error message.
+                Console.WriteLine(e);
+            }
+		}
+		
+		// Event handler for getting more events from DB.
+		private void UpcomingEventsFilter_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+		{
+			// TODO: Get events based on selected month.
+		}
+		
+		// Event handler for 'Previous' button.
+		private void PrevButton_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			// TODO: Get events from the previous month.
+		}
+
+		// Event handler for 'Next' button.
+		private void NextButton_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			// TODO: Get events from the next month.
+		}
+		
+		// Event handler when user clicks on 'Login' button.
+		private void LoginButton_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			// TODO: Validate user login information.
+			this.loginLoadingPanel.Visibility = Visibility.Visible;
+		}
+	}
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,7 +11,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.IO;
 using System.Collections.ObjectModel;
 using EventClass;
 using StudentClass;
@@ -23,62 +22,10 @@ namespace EMP
     /// </summary>
     public partial class MainWindow : Window
     {
-        private ObservableCollection<Event> eventList = new ObservableCollection<Event>();
-
-        public MainWindow()
-        {
-            //this.importEvents
-            this.importEventsFromFile();
-            InitializeComponent();
-        }
-
-        public ObservableCollection<Event> EventList
-        {
-            get { return this.eventList; }
-        }
-        
-        //import event from database
-        public void importEvents()
-        {
-        }
-        
-        private void importEventsFromFile()
-        {
-            try
-            {
-                StreamReader sr = new StreamReader("events.txt");
-                while (!sr.EndOfStream)
-                {
-                    int id = Convert.ToInt16(sr.ReadLine());
-                    string name = sr.ReadLine();
-                    string description = sr.ReadLine(); 
-                    string startDate = sr.ReadLine();
-                    string organizerName = sr.ReadLine();
-                    
-                    Event i = new Event(id, name, description, startDate, organizerName);
-                    this.EventList.Add(i);
-                }
-            }
-            catch (IOException e)
-            {
-                //display some error message
-                Console.WriteLine(e);
-            }
-        }
-
-        public void button1_Click(object sender, RoutedEventArgs e)
-        {
-            if (login(userIdBox.Text, passwordBox.Password))
-            {
-                Student s = new Student(userIdBox.Text, passwordBox.Password);
-                EventListWindow window = new EventListWindow();
-                window.student = s;
-                window.Show();
-                this.Close();
-            }
-            else
-                errorLabel.Visibility = Visibility.Visible;
-        }
-
+		public MainWindow()
+		{
+			this.InitializeComponent();
+			this._mainFrame.Navigate(new Uri("LoginScreen.xaml", UriKind.Relative));
+		}
     }
 }
